f99687dfbc8ba7210e76b5203b83297ed5510455
chengkangzai <pycck@hotmail.com>
2020-09-10 13:37:34 +0800

    Initial Commit

---
+++ Week 1.R
@@ -0,0 +1,8 @@
+#comment 
+install.packages() #install new packages
+installed.packages() #to check installed package
+search() #list down what software is installed
+library() #to load the package
+detach() #unload package
+remove.packages()#to uninstall package
+#ctrl-l --> clear console
