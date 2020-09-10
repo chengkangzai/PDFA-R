c6dcbde8aedd401533454de75727dd32eb3874ef
chengkangzai <pycck@hotmail.com>
2020-09-10 14:21:43 +0800

    Regular update

---
+++ L-10092020.R
@@ -0,0 +1,42 @@
+
+4/2+3*1-2*2
+
+assign('j',4)
+
+rm() #remove variable
+ls() #list all variable
+is.numeric()
+i = 5L
+#i = 5L
+is.integer(i)
+#STring
+x="welcome"
+x=4
+nchar(x)
+#Date
+date=as.Date("2020-09-10")
+class(date)
+#Logical
+k= TRUE
+class(k)
+
+a = 10
+b="c"
+c="coconut"
+d=FALSE
+e=10.1
+f=1L
+class(a)
+class(b)
+class(c)
+class(d)
+class(e)
+class(f)
+is.numeric(f)
+is.integer(f)
+as.character(e) #convert data type
+
+
+
+
+
