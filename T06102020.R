f5b6a639e45573646b7cc90beed24b1312bc6ac5
chengkangzai <pycck@hotmail.com>
2020-10-06 17:15:14 +0800

    update

--- T06102020.R
+++ T06102020.R
@@ -1,24 +1,28 @@
 radius = as.integer(readline(prompt = "Please give me a radius :"))
 
-switch (as.character(readline(prompt = "A for Area , C for Circumference , D for Diameter")),
-  "A" = message(c("Area : ",functionA())),
-  "C" = message(c("Circumference : ",functionC())),
-  "D" = message(c("Diameter : ",functionD())),
+switch (
+  as.character(readline(prompt = "A for Area , C for Circumference , D for Diameter")),
+  "A" = message(c("Area : ", functionA())),
+  "C" = message(c("Circumference : ", functionC())),
+  "D" = message(c("Diameter : ", functionD())),
   print("NO!")
 )
 
-functionA = function(){
-  return(radius*2)
+functionA = function() {
+  return(radius * 2)
 }
-functionC = function(){
-  return((2*pi)*radius)
+functionC = function() {
+  return((2 * pi) * radius)
 }
-functionD= function(){
-  return(pi*(radius*radius))
+functionD = function() {
+  return(pi * (radius * radius))
 }
 
 #############################################################################
+paste(functionReverse(as.character(
+  readline(prompt = "Give me sth! i will reverse it")
+)), collapse = '')
 
-
-input =as.character(readline(prompt = "Give me sth! i will reverse it"))
-
+functionReverse = function(input) {
+  return(rev(strsplit(input, NULL)[[1]]))
+}
