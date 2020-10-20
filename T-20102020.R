2a687f2312fcb1cf8816dfa3129657e940065069
chengkangzai <pycck@hotmail.com>
2020-10-20 17:22:40 +0800

    update

---
+++ T-20102020.R
@@ -0,0 +1,41 @@
+#Mary Ting
+##Q1
+
+vector1 = c(1:5)
+vector2 = vector()
+for (i in 1:5) {
+  vector2 = c(vector2, i)
+}
+
+##Q2
+x = sample(1:50, replace = TRUE)
+min(x)
+max(x)
+mean(x)
+sum(x)
+length(x)
+
+## Q3
+mat = matrix(1:12, nrow = 3, ncol = 4)
+
+##Q4
+nrow(mat)
+sum(mat[, 2])
+mat[3, 2] = 100
+rbind(mat, sample(50:100, size = 4))
+sum(mat)
+
+## Q5
+mylist = list(mat, x)
+
+## Q6
+myarray = array(dim = c(3, 5), data = c(c(1, 2, 3), c(3, 4, 5), c(6, 7, 8)))
+myarray[1, 3]
+
+
+## Q7
+weight = sample(x = 40:100, size = 10)
+age = sample(x = 1:70, size = 10)
+gender = sample(x = c("MALE", "FEMALE"),
+                size = 10,
+                replace = TRUE)
